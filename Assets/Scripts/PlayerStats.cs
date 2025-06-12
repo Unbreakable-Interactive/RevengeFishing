a513f2b1e7d660f6142c5f1b773bc702855ffa01
Steven Feldman <sielstarf@gmail.com>
2025-06-11 19:07:25 -0500

    Optimized player stats script

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -2,20 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+enum Phase { infant, juvenile, adult, beast, monster }
+
 public class PlayerStats : MonoBehaviour
 {
-    [SerializeField] private float powerLevel = 100; //default starting power level is 100
-    private float starterLevel;
-    /*[SerializeField]*/ private float hunger; //player dies if reaches 100%
-    /*[SerializeField]*/ private float fatigue; //player dies if reaches 100%
-    private enum Phase { infant, juvenile, adult, beast, monster }
+    [SerializeField] private long powerLevel = 100; //default starting power level is 100
+    private long starterLevel;
+    /*[SerializeField]*/ private long hunger; //player dies if reaches 100%
+    /*[SerializeField]*/ private long fatigue; //player dies if reaches 100%
     [SerializeField] private Phase phase = Phase.infant;
 
     // Start is called before the first frame update
     void Start()
     {
         starterLevel = powerLevel;
-        SetPhase(powerLevel);
+        SetPhase();
     }
 
     // Update is called once per frame
@@ -24,12 +25,12 @@ public class PlayerStats : MonoBehaviour
         
     }
 
-    void SetPhase(float powLevel)
+    void SetPhase()
     {
-        if (powLevel >= 10000000000) phase = Phase.monster; //modify these to be dynamic instead of hardcoded
-        else if (powLevel >= 100000000) phase = Phase.beast;
-        else if (powLevel >= 1000000) phase = Phase.adult;
-        else if (powLevel >= 10000) phase = Phase.juvenile;
+        if (powerLevel >= 10000000000) phase = Phase.monster; //modify these to be dynamic instead of hardcoded
+        else if (powerLevel >= 100000000) phase = Phase.beast;
+        else if (powerLevel >= 1000000) phase = Phase.adult;
+        else if (powerLevel >= 10000) phase = Phase.juvenile;
         else phase = Phase.infant;
     }
 }
