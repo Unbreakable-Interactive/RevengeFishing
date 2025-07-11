383ce4d7f545cbd9159723e0e32ed60a83a4461b
Steven Feldman <sielstarf@gmail.com>
2025-07-10 20:35:12 -0500

    Turned properties fatigue and maxFatigue into fields

--- Assets/Scripts/Entity/EntityFatigue.cs
+++ Assets/Scripts/Entity/EntityFatigue.cs
@@ -7,8 +7,8 @@ using UnityEngine;
 [System.Serializable]
 public class EntityFatigue
 {
-    [SerializeField] public int fatigue { get; set; }
-    [SerializeField] public int maxFatigue { get; set; }
+    [SerializeField] public int fatigue;
+    [SerializeField] public int maxFatigue;
 
     public EntityFatigue(int maxFatigue, int initFatigue = 0)
     {
